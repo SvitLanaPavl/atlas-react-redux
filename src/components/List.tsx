af4b35cc9c13f3dcd13f8456d16a366ac2c3d9c5
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-09-18 11:36:00 -0500

    connected redux to my list and new card components, use dispatcher, updated the props as previously there was no id, and added handlers to react on submit, and states to react to user input in title and description parts

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,19 +4,20 @@ import NewCardForm from './NewCardForm';
 import DeleteListButton from './DeleteListButton';
 
 interface ListProps {
+  id: string;
   title: string;
-  cards: { title: string; description: string }[];
+  cards: { id: string; title: string; description: string }[];
 }
 
-const List: React.FC<ListProps> = ({ title, cards }) => {
+const List: React.FC<ListProps> = ({ id, title, cards }) => {
   return (
     <div className="group/list h-full min-w-96 p-4 flex-1">
       <DeleteListButton />
       <h3>{title}</h3>
-      {cards.map((card, index) => (
-        <Card key={index} title={card.title} description={card.description} />
+      {cards.map((card) => (
+        <Card key={card.id} title={card.title} description={card.description} />
       ))}
-      <NewCardForm />
+      <NewCardForm listId={id} />
     </div>
   );
 };
--- src/components/NewCardForm.tsx
+++ src/components/NewCardForm.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addCard } from '../slices/listsSlice';
 
-const NewCardForm: React.FC = () => {
+interface NewCardFormProps {
+  listId: string;
+}
+
+const NewCardForm: React.FC<NewCardFormProps> = ( { listId } ) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (title.trim() && description.trim()) {
+      dispatch(addCard({ listId, title, description }));
+      setTitle('');
+      setDescription('');
+    }
+  };
+
+
   return (
     <div className="group/new-card m-3 flex h-44 w-full justify-center">
       <form
-        onSubmit={() => alert('Create card')}
+        onSubmit={handleSubmit}
         className="hidden min-h-24 w-full flex-col items-start rounded bg-off-white-light px-4 text-blue group-hover/new-card:flex"
       >
         <input
@@ -13,11 +33,15 @@ const NewCardForm: React.FC = () => {
           type="text"
           placeholder="Title"
           name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <textarea
           className="w-11/12 resize-none overflow-auto border-0 bg-off-white-light text-blue outline-none"
           placeholder="Description"
           name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         ></textarea>
         <div className="w-full">
           <button type="submit" className="w-full p-4">
